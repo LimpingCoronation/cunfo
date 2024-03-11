0d79ec7b03997ab213615633a969bb2a1d655f4a
LimpingCoronation <as.voronin8@gmail.com>
2024-03-11 12:54:56 +0300

    Made some changes

--- cunfo/settings.py
+++ cunfo/settings.py
@@ -98,7 +98,7 @@ if USE_POSTGRES:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': DB_NAME,
-            'USER': DB_HOST,
+            'USER': DB_USER,
             'PASSWORD': DB_PASSWORD,
             'HOST': DB_HOST,
             'PORT': DB_PORT,
